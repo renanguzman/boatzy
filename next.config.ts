782b600acf51c5c68afe6f06e06501e4f73a9990
Renan Guzman <renanguzman@gmail.com>
2026-06-18 12:13:08 -0300

    ajuste sso facebook

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com',
+      },
+      {
+        protocol: 'https',
         hostname: '*.r2.dev',
       },
     ],
